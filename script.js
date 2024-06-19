c8d6ef3c252801de55e3d67d1b420f95226d9963
Luís Teixeira <84951329+LuisT-ls@users.noreply.github.com>
2024-06-19 20:05:31 +0000

    implementação da bibliotexa AOS

--- script.js
+++ script.js
@@ -114,3 +114,12 @@ orcamentoBtn.addEventListener('click', event => {
     })
   }
 })
+
+const form = document.getElementById('contact-form')
+
+form.addEventListener('submit', function (event) {
+  if (!form.checkValidity()) {
+    event.preventDefault()
+    form.classList.add('was-validated')
+  }
+})
